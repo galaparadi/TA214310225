3ab55ff8a4fe761e300bde622181677a596b5311
galaparadi <gala_koyin@yahoo.co.id>
2021-07-05 07:59:04 +0700

    refactor auth router

--- routes/auth.js
+++ routes/auth.js
@@ -3,15 +3,16 @@ var passport = require('passport');
 var router = express.Router();
 const UserController = require('../controller/user');
 
-router.get('/', (res,req) => {
+router.get('/', (res, req) => {
 	res.redirect('/');
 });
 
-router.post('/auth/login', UserController.localLogin);
-router.get('/auth/logout', UserController.logout);
-//router.get('/auth/gdrive', null);
-router.get('/auth/google', passport.authenticate('google',{ 
-	scope: ['profile', 'email', 'https://www.googleapis.com/auth/drive.file']}));
-router.get('/auth/google/redirect', UserController.googleRedirect);
+router.post('/auth/login', UserController.localLogin)
+	.get('/auth/logout', UserController.logout)
+	//.get('/auth/gdrive', null)
+	.get('/auth/google', passport.authenticate('google', {
+		scope: ['profile', 'email', 'https://www.googleapis.com/auth/drive.file']
+	}))
+	.get('/auth/google/redirect', UserController.googleRedirect)
 
 module.exports = router;
