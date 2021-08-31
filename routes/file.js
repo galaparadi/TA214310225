fb3c5cbdcde7c9a1477d6a9bdde5373e33ae55c3
galaparadi <gala_koyin@yahoo.co.id>
2021-08-31 08:31:39 +0700

    implement : upload from google drive

--- routes/file.js
+++ routes/file.js
@@ -27,13 +27,22 @@ router.get('/download/:workspace/:name', async (req, res, next) => {
 
 router.post('/upload', upload.single('document'), async (req, res, next) => {
 	let form = new FormData();
+	if (req.file) form.append('file', req.file.buffer, req.body['file-name']);
 	form.append('workspace', req.body.workspace);
 	form.append('filename', req.body['filename']);
 	form.append('author', req.user._id);
-	form.append('file', req.file.buffer, req.body['file-name']);
-	form.append('author-level', req.body['author-level'])
+	form.append('author-level', req.body['author-level']);
+	form.append('file-id', req.body['drive-id']);
+	form.append('refresh-token', req.body['refresh-token']);
+	form.append('access-token', req.body['access-token']);
+	form.append('mime-type', req.body['mime-type']);
 	try {
-		let data = (await WorkspaceDatasource.addDocument({ form, workspace: req.body.workspace, authorLevel: req.body['author-level'] })).data;
+		let data = await WorkspaceDatasource.addDocument({ form, workspace: req.body.workspace, authorLevel: req.body['author-level'] });
+		if (data.status) {
+			res.cookie('addDocument', data.message, { expires: new Date(Date.now() + 1000) });
+		} else {
+			res.cookie('errAddDocument', data.message, { expires: new Date(Date.now() + 1000) })
+		}
 		res.redirect(`/${req.body.workspace}`);
 	} catch (error) {
 		console.log(error);
@@ -48,11 +57,11 @@ router.post('/version', upload.single('file'), async (req, res, next) => {
 		form.append('documentId', req.body.documentId);
 		form.append('filename', req.body['file-name']);
 		form.append('author', req.user._id);
-		form.append('file', req.file.buffer, {filename:req.body['file-name'], dudung: 'dudung'});
+		form.append('file', req.file.buffer, { filename: req.body['file-name'], dudung: 'dudung' });
 		let data = (await WorkspaceDatasource.addDocumentVersion({ form, workspace: req.body.workspace, documentId: req.body.documentId }));
 		res.redirect(`/${req.body.workspace}`);
 	} catch (error) {
-		console.log({message: error.message, stack : error.stack});
+		console.log({ message: error.message, stack: error.stack });
 		next(error);
 	}
 })
